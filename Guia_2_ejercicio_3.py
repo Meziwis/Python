3a718dda27838426af5cd473888fa8987db106bf
Grupo4IgnacioNicolas <66972969+Grupo4IgnacioNicolas@users.noreply.github.com>
2020-06-15 19:08:20 -0400

    Create Guia_2_ejercicio_3.py

---
+++ Guia_2_ejercicio_3.py
@@ -0,0 +1,12 @@
+a = 2
+while a<=14:
+    print(a)
+    a+=2
+    if a ==16:
+        print('Adios!')
+        
+for i in range(0,10,2):
+    if i ==0:
+        print('"hello"')
+    print(10-i)
+    
